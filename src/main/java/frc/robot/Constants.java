7f36c5fb96088370d514f44d629feeedefe4eb6a
Hannah Thompson <122500450+NahCaprisun@users.noreply.github.com>
2024-02-05 18:40:23 -0600

    debugging/creating stuff

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,100 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
+import edu.wpi.first.math.util.Units;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants should be declared
+ * globally (i.e. public static). Do not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class Constants {
+
+  public static final class DrivetrainConstants {
+    //according to CAN IDs
+    public static final int frontLeftMotor = 1;
+    public static final int frontRightMotor = 2;
+    public static final int backLeftMotor = 3;
+    public static final int backRightMotor = 4;
+
+    //idk where this number came from but it's been the same for the past 2 years
+    public static final double rampRate = 0.5;
+    public static double kDistancePerWheelRevolutionMeters = Units.inchesToMeters(6*Math.PI);
+    public static double kGearReduction = 10.71;
+    public static double ksVolts = 0.15239;
+    public static double kvVoltSecondsPerMeter = 2.7742;
+    public static double kaVoltSecondsSquaredPerMeter = 0.64165;
+    public static double kPDriveVel = 0;
+    public static DifferentialDriveKinematics kDriveKinematics=new DifferentialDriveKinematics(Units.inchesToMeters(22));
+    public static double kRamseteB = 2.0;
+    public static double kRamseteZeta = 0.7;
+
+  
+    public static double balancePowerMin = 0.1;
+    public static double balancePowerMax = 0.35;
+    public static double balanceDistance = 1.65;
+    public static double balanceDistanceShort = 1.3;
+    public static double autoTurnPower = 0.3;
+    public static double mobilityDistance = 3.8;
+  }
+  public static final class ClimberConstants{
+    public static final int rightClawMotor = 8;
+    public static final double rightClawMotorPower = .2;
+    public static final int leftClawMotor = 9;
+    public static final double leftClawMotorPower = .2;
+
+    //figure out how to get the limits set up
+  }
+
+  public static final class IntakeConstants {
+    public static final int intakeMotor = 5;
+    public static final double intakeMotorPower = 0.5;
+    public static final int pivotMotor = 6;
+    public static final double pivotMotorPower = 0.2;
+    public static final int armMotor = 7;
+    public static final double armMotorPower = 0.2;
+
+    public static final float pivotLimitIn = 0.0f;
+    public static final float pivotLimitOut = 28.0f; // this will be negated in the subsystem because negative means out
+    public static final float armLimitIn = 0.0f;
+    public static final float armLimitOut = 9.5f; // this will remain positive in the subsystem because positive means out
+  }
+
+  public static final class JoystickConstants {
+        //Controllers
+        public static final int DRIVER_PORT = 0;
+        public static final int OPERATOR_PORT = 1;
+            
+        //XboxOne Joysticks (axes)
+        public static final int LEFT_STICK_X = 0;
+        public static final int LEFT_STICK_Y = 1;
+        public static final int LEFT_TRIGGER = 2;
+        public static final int RIGHT_TRIGGER = 3;
+        public static final int RIGHT_STICK_X = 4;
+        public static final int RIGHT_STICK_Y = 5;
+
+        public static final double deadband = 0.1;
+        
+        //XboxOne Buttons
+        public static final int A = 1;
+        public static final int B = 2;
+        public static final int X = 3;
+        public static final int Y = 4;
+        public static final int BUMPER_LEFT = 5;
+        public static final int BUMPER_RIGHT = 6;
+        public static final int LOGO_LEFT = 7;
+        public static final int LOGO_RIGHT = 8;
+        public static final int LEFT_STICK_BUTTON = 9;
+        public static final int RIGHT_STICK_BUTTON = 10;
+  }
+  public static class OperatorConstants {
+    public static final int kDriverControllerPort = 0;
+  }
+}
---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,90 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import frc.robot.Constants.JoystickConstants;
+import frc.robot.Constants.OperatorConstants;
+//import frc.robot.commands.Autos;
+import frc.robot.subsystems.LeftClaw;
+import frc.robot.subsystems.RightClaw;
+import frc.robot.subsystems.Drive_Train;
+import frc.robot.commands.ArcadeDrive;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and trigger mappings) should be declared here.
+ */
+public class RobotContainer {
+  // The robot's subsystems and commands are defined here...
+  private final Drive_Train _drive_Train = new Drive_Train();
+  private final Joystick _driver = new Joystick(0);
+  private final Joystick _operator = new Joystick(1);
+
+  private final LeftClaw _leftClaw = new LeftClaw();
+  private final RightClaw _rightClaw = new RightClaw();
+
+  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  public RobotContainer() {
+    // Configure the trigger bindings
+    configureBindings();
+    _drive_Train.setDefaultCommand(new ArcadeDrive(_drive_Train, _driver));
+  }
+
+  /**
+   * Use this method to define your trigger->command mappings. Triggers can be created via the
+   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
+   * predicate, or via the named factories in {@link
+   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
+   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
+   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
+   * joysticks}.
+   */
+  private void configureBindings() {
+    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
+    //configure when have commands
+    // new JoystickButton(_operator, JoystickConstants.BUMPER_RIGHT)
+    //   .onTrue(new InstantCommand(() -> _bootyIntake.setState(BootyState.CubeIntake)))
+    //   .onFalse(new InstantCommand(() -> _bootyIntake.setState(BootyState.CubeHold))); 
+    // new JoystickButton(_operator, JoystickConstants.BUMPER_LEFT)
+    //   .onTrue(new InstantCommand(() -> _bootyIntake.setState(BootyState.ConeIntake)))
+    //   .onFalse(new InstantCommand(() -> _bootyIntake.setState(BootyState.ConeHold)));
+    
+    new JoystickButton(_operator, JoystickConstants.BUMPER_RIGHT).whileTrue(new RunCommand(_rightClaw::rightClawUp, _rightClaw));
+    new JoystickButton(_operator, JoystickConstants.RIGHT_TRIGGER).whileTrue(new RunCommand(_rightClaw::rightClawDown,_rightClaw));
+    new JoystickButton(_operator, JoystickConstants.BUMPER_LEFT).whileTrue(new RunCommand(_leftClaw::leftClawUp, _leftClaw));
+    new JoystickButton(_operator, JoystickConstants.LEFT_TRIGGER).whileTrue(new RunCommand(_leftClaw::leftClawDown, _leftClaw));
+      //new JoystickButton(_operator, JoystickConstants.Y).whileTrue(new RunCommand(_fourBarArms::armOut, _fourBarArms));
+    // new JoystickButton(_operator, JoystickConstants.A).whileTrue(new RunCommand(_fourBarArms::armIn, _fourBarArms));
+    // new JoystickButton(_operator, JoystickConstants.X).whileTrue(new RunCommand(_intakePivot::pivotUp, _intakePivot));
+    // new JoystickButton(_operator, JoystickConstants.B).whileTrue(new RunCommand(_intakePivot::pivotDown, _intakePivot));
+    
+    // new Trigger(m_dualClaw::exampleCondition)
+    //     .onTrue(new LeftClaw(m_dualClaw));
+
+    // // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
+    // // cancelling on release.
+    // m_driverController.b().whileTrue(m_dualClaw.exampleMethodCommand());
+  }
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   
+  public Command getAutonomousCommand() {
+    // An example command will be run in autonomous
+    return Autos.exampleAuto(dualClaw);
+  }
+  */
+}
