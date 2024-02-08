d55928e1c0b1f47d05990fd98f69286b5e7610e8
FRC-PlatyPirates-9181 <team@platypirates.org>
2024-02-07 20:19:47 -0600

    more changing constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -51,9 +51,9 @@ public final class Constants {
     
     //figure out how to get the limits set up
     public static final float rightClawLimitIn = 0.923110f;//figure ths out
-    public static final float rightClawLimitOut = 0.0f; // this will be negated in the subsystem because negative means out
+    public static final float rightClawLimitOut = 0.02380953f; // this will be negated in the subsystem because negative means out
     public static final float leftClawLimitIn = 0.923110f; //figure this out too
-    public static final float leftClawLimitOut = 0.0f;
+    public static final float leftClawLimitOut = 0.04761905f;
   }
 
   public static final class IntakeConstants {
